fbdd6df199f0ba6548a80d30526dddae04595c3a
meirf <meirusfandi100@gmail.com>
2022-06-19 23:08:12 +0700

    update on game play

--- app/src/main/java/com/example/rockpaperscissors/LeaderboardAdapter.kt
+++ app/src/main/java/com/example/rockpaperscissors/LeaderboardAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.rockpaperscissors
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.ViewTarget
 import com.example.rockpaperscissors.databinding.ItemLeaderboardBinding
 import com.example.rockpaperscissors.model.Player
 
-class LeaderboardAdapter : RecyclerView.Adapter<LeaderboardAdapter.LeaderBoardViewHolder>() {
+class LeaderboardAdapter: RecyclerView.Adapter<LeaderboardAdapter.LeaderBoardViewHolder>() {
 
     private val listPlayer: MutableList<Player> = mutableListOf()
 
     fun addList(players: List<Player>) {
         listPlayer.clear()
         listPlayer.addAll(players)
-        notifyDataSetChanged()
     }
 
     class LeaderBoardViewHolder(private val binding: ItemLeaderboardBinding) :
@@ -21,6 +25,10 @@ class LeaderboardAdapter : RecyclerView.Adapter<LeaderboardAdapter.LeaderBoardVi
         fun bind(player: Player) = with(binding) {
             itemTvName.text = player.name
             itemTvScore.text = "Score : ${player.score}"
+//            Glide.with(itemView.context)
+//                .load(player.avatar)
+//                .apply(RequestOptions().override(60, 60))
+//                .into(imgAvatar)
         }
     }
 
--- app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -16,17 +16,31 @@ import com.example.rockpaperscissors.action.KertasAction
 import com.example.rockpaperscissors.databinding.ActivityMainBinding
 import com.example.rockpaperscissors.fragment.WinLoseDialogFragment
 import com.example.rockpaperscissors.model.Player
-import com.example.rockpaperscissors.presenter.InsertPresenter
-import com.example.rockpaperscissors.presenter.InsertPresenterImpl
+import com.example.rockpaperscissors.presenter.*
+import com.example.rockpaperscissors.sources.PlayerDao
+import com.example.rockpaperscissors.sources.PlayerDatabase
+import com.example.rockpaperscissors.view.CheckNameView
 import com.example.rockpaperscissors.view.InsertView
+import com.example.rockpaperscissors.view.UpdateView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.util.*
 
-class MainActivity : AppCompatActivity(), InsertView {
+class MainActivity : AppCompatActivity(), InsertView, CheckNameView, UpdateView {
 
     private val handler = Handler()
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
+
+    private val checkNamePresenter: CheckNamePresenter = CheckNamePresenterImpl(this)
+    private val updatePresenter: UpdatePresenter = UpdatePresenterImpl(this)
+    private val playerDatabase: PlayerDatabase? by lazy {
+        PlayerDatabase.getInstance(this)
+    }
+    private val playerDao: PlayerDao? by lazy {
+        playerDatabase?.playerDao()
+    }
 
     private val insertPresenter: InsertPresenter = InsertPresenterImpl(this)
 
@@ -46,6 +60,14 @@ class MainActivity : AppCompatActivity(), InsertView {
         intent.getStringExtra(ENEMY_TYPE).toString()
     }
 
+    private val dialogWinLoseFragment = WinLoseDialogFragment()
+    val bundle = Bundle()
+
+    private var picked = false
+    private var player1pick: Suit? = null
+    private var player2pick: String? = null
+    private var gameDone = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -54,111 +76,100 @@ class MainActivity : AppCompatActivity(), InsertView {
         binding?.tvPlayerName?.text = playerName
         binding?.tvOpponentName?.text = enemyName
 
-        val compChoice = listOf(binding?.enemyRock, binding?.enemyPaper, binding?.enemyScissors)
-
-        fun activateColor(choice: CardView) {
-            choice.setCardBackgroundColor(Color.parseColor("#C3DAE9"))
-        }
-
-        fun disableColor(choice: CardView) {
-            choice.setCardBackgroundColor(Color.parseColor("#FFFFFF"))
-        }
-
-        val dialogWinLoseFragment = WinLoseDialogFragment()
-        val bundle = Bundle()
-
-        fun showMessage(condition: String) {
-            when (condition) {
-                "win" -> {
-                    binding?.tvResult?.visibility = View.VISIBLE
-                    binding?.tvVs?.visibility = View.GONE
-                    bundle.putString(PLAYER_NAME, playerName)
-                    val newPlayer = Player(
-                        name = playerName,
-                        score = 5,
-                        avatar = playerAvatar
-                    )
-//                    insertPresenter.saveToDatabase(newPlayer)
-                }
-                "lose" -> {
-                    binding?.tvResult?.visibility = View.VISIBLE
-                    binding?.tvVs?.visibility = View.GONE
-                    bundle.putString(PLAYER_NAME, enemyName)
-                    val newPlayer = Player(
-                        name = enemyName,
-                        score = 5,
-                        avatar = enemyAvatar
-                    )
-//                    insertPresenter.saveToDatabase(newPlayer)
-                }
-                else -> {
-                    binding?.tvVs?.text = getString(R.string.draw)
-                    binding?.tvVs?.visibility = View.VISIBLE
-                }
-            }
-        }
-
-        fun playerClick(choice: CardView? = null) {
-            choice?.let {
-                val suitUser: Suit = when (choice) {
-                    binding?.playerRock -> BatuAction()
-                    binding?.playerPaper -> KertasAction()
-                    else -> GuntingAction()
-                }
-                val suitCom = DataSources.getRandomSuit()
-                var delay: Long = 0
-                for (i in 0..2) {
-                    compChoice.map {
-                        handler.postDelayed({
-                            if (it != null) {
-                                activateColor(it)
-                            }
-                        }, delay)
-                        delay += 500
-                        handler.postDelayed({
-                            if (it != null) {
-                                disableColor(it)
-                            }
-                        }, delay)
-                    }
-                }
-                handler.postDelayed({
-                    when (suitCom.name) {
-                        "batu" -> activateColor(binding!!.enemyRock)
-                        "kertas" -> activateColor(binding!!.enemyPaper)
-                        else -> activateColor(binding!!.enemyScissors)
-                    }
-                    val toastMessage = "$enemyName Memilih " + suitCom.name.replaceFirstChar {
-                        if (it.isLowerCase()) it.titlecase(
-                            Locale.ROOT
-                        ) else it.toString()
-                    }
-                    Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-                    val result = suitUser.action(suitCom.name)
-
-                    showMessage(result)
-
-                    bundle.putString(RESULT, result)
-                    dialogWinLoseFragment.arguments = bundle
-                    dialogWinLoseFragment.show(supportFragmentManager, null)
-                }, delay)
-            }
-        }
-
+        // player press rock section
         binding?.playerRock?.setOnClickListener {
-            playerClick(binding?.playerRock)
+            if (enemyType == "player") {
+                if (!gameDone) {
+                    if (player1pick == null) {
+                        Toast.makeText(this, "$playerName Memilih Batu", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+                }
+                playerClick(binding!!.playerRock)
+            } else {
+                playerVsCompClick(binding?.playerRock)
+            }
             Toast.makeText(this, "$playerName Memilih Batu", Toast.LENGTH_SHORT).show()
         }
+        // end section
 
+        // player press paper section
         binding?.playerPaper?.setOnClickListener {
-            playerClick(binding?.playerPaper)
+            if (enemyType == "player") {
+                if (!gameDone) {
+                    if (player1pick == null) {
+                        Toast.makeText(this, "$playerName Memilih Kertas", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+                }
+                playerClick(binding!!.playerPaper)
+            } else {
+                playerVsCompClick(binding?.playerPaper)
+            }
             Toast.makeText(this, "$playerName Memilih Kertas", Toast.LENGTH_SHORT).show()
         }
+        // end section
 
+        // player press scissors section
         binding?.playerScissors?.setOnClickListener {
-            playerClick(binding?.playerScissors)
+            if (enemyType == "player") {
+                if (!gameDone) {
+                    if (player1pick == null) {
+                        Toast.makeText(this, "$playerName Memilih Gunting", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+                }
+                playerClick(binding!!.playerScissors)
+            } else {
+                playerVsCompClick(binding?.playerScissors)
+            }
             Toast.makeText(this, "$playerName Memilih Gunting", Toast.LENGTH_SHORT).show()
         }
+        // end section
+
+        // opponent press rock section
+        binding?.enemyRock?.setOnClickListener {
+            if (!gameDone) {
+                if (player2pick == null) {
+                    Toast.makeText(this, "$enemyName Memilih Batu", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+            }
+            opponentClick(binding!!.enemyRock)
+        }
+        // end section
+
+        // opponent press paper section
+        binding?.playerPaper?.setOnClickListener {
+            if (!gameDone) {
+                if (player2pick == null) {
+                    Toast.makeText(this, "$enemyName Memilih Kertas", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+            }
+            opponentClick(binding!!.enemyPaper)
+        }
+        // end section
+
+        // opponent press scissors section
+        binding?.playerScissors?.setOnClickListener {
+            if (!gameDone) {
+                if (player2pick == null) {
+                    Toast.makeText(this, "$enemyName Memilih Gunting", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Tekan Reset Dulu!", Toast.LENGTH_SHORT).show()
+            }
+            opponentClick(binding!!.enemyScissors)
+        }
+        // end section
 
         binding?.btnRefresh?.setOnClickListener {
             resetView()
@@ -170,21 +181,215 @@ class MainActivity : AppCompatActivity(), InsertView {
         }
     }
 
+    private fun showMessage(condition: String) {
+        when (condition) {
+            "win" -> {
+                binding?.tvResult?.visibility = View.VISIBLE
+                binding?.tvResult?.text = "$playerName Menang"
+                binding?.tvVs?.visibility = View.GONE
+                bundle.putString(PLAYER_NAME, playerName)
+                if (enemyType == "player") {
+                    GlobalScope.launch {
+                        val player = playerDao?.getPlayerByName(playerName)?.first()
+                        runOnUiThread {
+                            if (player != null) {
+                                if (player.id!! > 0) {
+                                    player.score += 5
+                                    updatePresenter.updateDatabase(player)
+                                } else {
+                                    val newPlayer = Player(
+                                        name = playerName,
+                                        score = 5,
+                                        avatar = playerAvatar
+                                    )
+                                    insertPresenter.saveToDatabase(newPlayer)
+                                }
+                            } else {
+                                val newPlayer = Player(
+                                    name = playerName,
+                                    score = 5,
+                                    avatar = playerAvatar
+                                )
+                                insertPresenter.saveToDatabase(newPlayer)
+                            }
+                        }
+                    }
+                }
+            }
+            "lose" -> {
+                binding?.tvResult?.visibility = View.VISIBLE
+                binding?.tvResult?.text = "$enemyName Menang"
+                binding?.tvVs?.visibility = View.GONE
+                bundle.putString(PLAYER_NAME, enemyName)
+                if (enemyType == "player") {
+                    GlobalScope.launch {
+                        val player = playerDao?.getPlayerByName(enemyName)?.first()
+                        runOnUiThread {
+                            if (player != null) {
+                                if (player.id!! > 0) {
+                                    player.score += 5
+                                    updatePresenter.updateDatabase(player)
+                                } else {
+                                    val newPlayer = Player(
+                                        name = enemyName,
+                                        score = 5,
+                                        avatar = enemyAvatar
+                                    )
+                                    insertPresenter.saveToDatabase(newPlayer)
+                                }
+                            } else {
+                                val newPlayer = Player(
+                                    name = enemyName,
+                                    score = 5,
+                                    avatar = enemyAvatar
+                                )
+                                insertPresenter.saveToDatabase(newPlayer)
+                            }
+                        }
+                    }
+                }
+            }
+            else -> {
+                binding?.tvResult?.visibility = View.GONE
+                binding?.tvVs?.text = getString(R.string.draw)
+                binding?.tvVs?.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    // section player click on Player VS Player
+    private fun playerClick(choice: CardView) {
+        if (gameDone || player1pick != null) {
+            return
+        }
+        choice.let {
+            activateColor(choice)
+            val suitPlayer1: Suit = when (choice) {
+                binding?.playerRock -> BatuAction()
+                binding?.playerPaper -> KertasAction()
+                else -> GuntingAction()
+            }
+            if (picked) {
+                player2pick?.let { it1 -> result(suitPlayer1, it1) }
+            } else {
+                player1pick = suitPlayer1
+                picked = true
+            }
+        }
+    }
+
+    private fun opponentClick(choice: CardView? = null) {
+        if (gameDone || player2pick != null) {
+            return
+        }
+        choice?.let {
+            activateColor(choice)
+            val suitUser: String = when (choice) {
+                binding?.enemyRock -> "batu"
+                binding?.enemyPaper -> "kertas"
+                else -> "gunting"
+            }
+            if (picked) {
+                player1pick?.let { it1 -> result(it1, suitUser) }
+            } else {
+                player2pick = suitUser
+                picked = true
+            }
+        }
+    }
+
+    private fun result(suitPlayer1: Suit, suitPlayer2: String) {
+        val result = suitPlayer1.action(DataSources.convertStringToData(suitPlayer2).name)
+        binding?.tvVs?.visibility = View.INVISIBLE
+        showMessage(result)
+        bundle.putString("RESULT", result)
+        dialogWinLoseFragment.arguments = bundle
+        dialogWinLoseFragment.show(supportFragmentManager, null)
+        gameDone = true
+    }
+
+    private fun playerVsCompClick(choice: CardView? = null) {
+        val compChoice = listOf(binding?.enemyRock, binding?.enemyPaper, binding?.enemyScissors)
+        choice?.let {
+            activateColor(choice)
+            val suitUser: Suit = when (choice) {
+                binding?.playerRock -> BatuAction()
+                binding?.playerPaper -> KertasAction()
+                else -> GuntingAction()
+            }
+            val suitCom = DataSources.getRandomSuit()
+            var delay: Long = 0
+            for (i in 0..2) {
+                compChoice.map {
+                    handler.postDelayed({
+                        if (it != null) {
+                            activateColor(it)
+                        }
+                    }, delay)
+                    delay += 500
+                    handler.postDelayed({
+                        if (it != null) {
+                            disableColor(it)
+                        }
+                    }, delay)
+                }
+            }
+            handler.postDelayed({
+                when (suitCom.name) {
+                    "batu" -> activateColor(binding!!.enemyRock)
+                    "kertas" -> activateColor(binding!!.enemyPaper)
+                    else -> activateColor(binding!!.enemyScissors)
+                }
+                val toastMessage = "$enemyName Memilih " + suitCom.name.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(
+                        Locale.ROOT
+                    ) else it.toString()
+                }
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+                val result = suitUser.action(suitCom.name)
+
+                showMessage(result)
+
+                bundle.putString(RESULT, result)
+                dialogWinLoseFragment.arguments = bundle
+                dialogWinLoseFragment.show(supportFragmentManager, null)
+            }, delay)
+        }
+    }
+
+    private fun activateColor(choice: CardView) {
+        choice.setCardBackgroundColor(Color.parseColor("#C3DAE9"))
+    }
+
+    private fun disableColor(choice: CardView) {
+        choice.setCardBackgroundColor(Color.parseColor("#FFFFFF"))
+    }
+
     @SuppressLint("UseCompatLoadingForDrawables")
-    private fun resetView() = with(binding){
-        this!!.playerRock.background = getDrawable(R.drawable.background_unselect)
-        this.playerPaper.background = getDrawable(R.drawable.background_unselect)
-        this.playerScissors.background = getDrawable(R.drawable.background_unselect)
-        this.enemyRock.background = getDrawable(R.drawable.background_unselect)
-        this.enemyPaper.background = getDrawable(R.drawable.background_unselect)
-        this.enemyScissors.background = getDrawable(R.drawable.background_unselect)
-        this.tvVs.visibility = View.VISIBLE
-        this.tvVs.text = getString(R.string.vs)
-        this.tvResult.visibility = View.GONE
+    private fun resetView() = with(binding) {
+        disableColor(this!!.playerRock)
+        disableColor(playerPaper)
+        disableColor(playerScissors)
+        disableColor(enemyRock)
+        disableColor(enemyPaper)
+        disableColor(enemyScissors)
+        tvVs.visibility = View.VISIBLE
+        tvVs.text = getString(R.string.vs)
+        tvResult.visibility = View.GONE
     }
 
     override fun context(): Context {
         return this
+    }
+
+    override fun onUpdateDatabase() {}
+
+    override fun onCheckDatabase(player: List<Player>) {}
+
+    override fun onResume() {
+        super.onResume()
+        checkNamePresenter.getPlayerByName(playerName)
+        checkNamePresenter.getPlayerByName(enemyName)
     }
 
     override fun onSaveDatabase() {}
